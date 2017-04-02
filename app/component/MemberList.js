45be4781bfb5922f35e84aa8c569abc5c776d8e9
Nuttaya Chutibuat <netty.ncz@gmail.com>
2017-04-02 22:29:27 +0700

    onDelete function , show create button

---
+++ app/component/MemberList.js
@@ -0,0 +1,66 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var styles = require('../styles');
+var ReactBsTable  = require('react-bootstrap-table')
+var _  = require('lodash')
+var BootstrapTable = ReactBsTable.BootstrapTable;
+var TableHeaderColumn = ReactBsTable.TableHeaderColumn;;
+var Link = ReactRouter.Link
+
+function onClickRow (e,id) {
+  return window.location = 'update?id='+id;
+}
+function onDelete(){
+  var r = confirm("Confirm to Delete");
+  if (r == true) {
+    console.log('s')
+  } else {
+    console.log('g')
+  }
+}
+
+var MemberList = React.createClass({
+	renderRows:function(){
+		return _.map(this.props.membersList, function(user,key){
+			return(
+				<tr key={key} >
+      		<td><img src={user.avatarUrl} alt="" className="img-profile-MemberList"/></td>
+      		<td className="td-link" onClick={(e) => onClickRow(e, user.id)}>{user.firstName} {user.lastName}</td>
+      		<td className="text-center">
+            <button className="btn btn-danger" onClick={onDelete}>
+              <i className="fa fa-trash" aria-hidden="true"></i>
+            </button>
+          </td>
+      	</tr>
+			)
+		})
+	},
+  render: function () {
+    return (
+    	<div>
+    	<h1>{this.props.route.header}</h1>
+    		<div className="form-group col-sm-12">
+    		<Link to="create">
+          <button type="submit" className="btn btn-success pull-right" style={ styles.marginbutton }>
+            <i className="fa fa-plus" aria-hidden="true"></i> Create
+          </button>
+        </Link>
+        </div>
+        <table className="table table-striped">
+        	<thead>
+        		<tr>
+	        		<th>#</th>
+	        		<th>Firstname Lastnames</th>
+	        		<th>#</th>
+	        	</tr>
+        	</thead>
+        	<tbody>
+        		{this.renderRows()}
+        	</tbody>
+        </table>
+			</div>
+    )
+  }
+});
+
+module.exports = MemberList;
--- app/containers/MemberListContainer.js
+++ app/containers/MemberListContainer.js
@@ -2,82 +2,45 @@ var React = require('react');
 var ReactRouter = require('react-router');
 var styles = require('../styles');
 var ReactBsTable  = require('react-bootstrap-table')
+var MemberList = require('../component/MemberList');
 var _  = require('lodash')
 var BootstrapTable = ReactBsTable.BootstrapTable;
 var TableHeaderColumn = ReactBsTable.TableHeaderColumn;;
 var Link = ReactRouter.Link
+const request = require('axios')
 
-const products = [
-	{
-    id: 1,
-    firstName: "Peter",
-    lastName : "C",
-  }, {
-    id: 2,
-    firstName: "James",
-    lastName : "C"
-  }, {
-    id: 3,
-    firstName: "Gram",
-    lastName : "C"
-  }, {
-    id: 4,
-    firstName: "Sand",
-    lastName : "C"
-  }, {
-    id: 5,
-    firstName: "John",
-    lastName : "C"
-  }
- ];
-
-function onClickRow (e) {
-  return window.location = 'update'
-}
-
-var Home = React.createClass({
-	renderRows:function(){
-		return _.map(products, function(product,key){
-			return(
-				<tr key={key} onClick={onClickRow}>
-      		<td>{product.id}</td>
-      		<td>{product.firstName} {product.lastName}</td>
-      		<td> x x</td>
-      	</tr>
-			)
-		})
-	},
+var MemberListContainer = React.createClass({
+	propTypes:{
+    route: React.PropTypes.shape({
+      header:React.PropTypes.string
+    })
+  },
+  getInitialState(){
+    return{
+      membersList:null
+    }
+  },
+  componentDidMount(){
+    var users
+    request.get('http://demo4925167.mockable.io/users')
+    .then((response) => {
+      const result = response.data
+      this.setState({
+        membersList: result
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  },
   render: function () {
     return (
-    	<div>
-    	<h1>{this.props.route.header}</h1>
-    		<div className="form-group col-sm-4 col-sm-offset-4">
-    		<Link to="create" className="hidden">
-          <button type="submit" className="btn btn-success" style={ styles.marginbutton }>Create</button>
-        </Link>
-        </div>
-        <table className="table table-striped">
-        	<thead>
-        		<tr>
-	        		<th>#</th>
-	        		<th>Firstname Lastnames</th>
-	        		<th>#</th>
-	        	</tr>
-        	</thead>
-        	<tbody>
-        		{this.renderRows()}
-        	</tbody>
-        </table>
-	    	{/*<BootstrapTable data={ products } insertRow={ true } deleteRow={ true } selectRow={ selectRowProp } options={ options }>
-          <TableHeaderColumn width='500' dataField='id' isKey>Product ID</TableHeaderColumn>
-          <TableHeaderColumn width='600' dataField='name'>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-      	</BootstrapTable>*/}
-
-	    	
-			</div>
+    	<MemberList 
+        route={this.props.route} 
+        membersList={this.state.membersList}
+      />
     )
   }
 });
 
-module.exports = Home;
+module.exports = MemberListContainer;
